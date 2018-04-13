d0610a1b4315542ecd9ce8922f9b5d3d76fed33f
Rémi DELIANCE <remi.deliance@decathlon.com>
2018-04-13 09:55:15 +0200

    exos3

--- 02_programming_fundamentals/08_this_day_1/02_chaining/02_car.js
+++ 02_programming_fundamentals/08_this_day_1/02_chaining/02_car.js
@@ -1,3 +1,32 @@
 // See Sparta courses for the exercise summary
+const car = {
+  speed: 0,
+  totalDistanceBitch: 0,
+  distance: 0,
+
+  start: function() {
+    this.speed = 0;
+    this.totalDistanceBitch = 0;
+    this.distance = 0;
+    return this;
+  },
+
+  changeSpeed: function(value1) {
+    this.speed = value1;
+    return this;
+  },
+
+  showDistance: function() {
+
+    return this.totalDistanceBitch;
+  },
+
+  drive: function(minutes) {
+    this.totalDistanceBitch += this.speed * minutes/60;
+    return this;
+  }
+};
+
+console.log(car.start().changeSpeed(120).drive(41).showDistance());
 
 module.exports = car;
--- 02_programming_fundamentals/08_this_day_1/03_implementing/03_coffee_machine.js
+++ 02_programming_fundamentals/08_this_day_1/03_implementing/03_coffee_machine.js
@@ -1,14 +1,92 @@
 // See Sparta courses for the exercise summary
 
-// Coffee Machine usage. Insert your code above this comment
+const machine {
+    LiterOfCoffe: 10,
+    kexpresso: 0.08,
+    klongCoffe: 0.15,
+
+    fillWithLitersOfCoffee: function(numberOfliters) {
+      this.LiterOfCoffe += numberOfliters;
+    },
+
+    expresso: function() {
+      if (this.LiterOfCoffe < 0,08) {
+        return false;
+      }
+      this.litersOfCoffee -= 0,8;
+      return true;
+    },
+
+    longCoffee: function() {
+      if (this.litersOfCoffe < 0,15) {
+        return false;
+      }
+      this.litersOfCoffe -= 0,15;
+      return true;
+    },
+  }};
+
+  machine.fillWithLitersOfCoffee(10);
+  console.log(machine.expresso()) // => true
+  console.log(machine.litersOfCoffee) // => 9.92
+  console.log(machine.longCoffee()) // => true
+  console.log(machine.litersOfCoffee) // => 9.77
+  // some more people ordering coffee here
+  console.log(machine.litersOfCoffee) // => 0.02
+  console.log(machine.expresso()) // => false
+
+  module.exports = machine;
+
+  /*
+  Correction
+  const machine = {
+
+  litersOfCoffee: 0 ,
+  expressoVolume: 0.08 ,
+  longCoffeeVolume: 0.15 ,
+
+
+  fillWithLitersOfCoffee : function (quantityOfCoffee){
+    this.litersOfCoffee = this.litersOfCoffee + quantityOfCoffee;
+    //this.litersOfCoffee += quantityOfCoffee;
+    return this;
+  },
+  expresso : function (){
+    if (this.litersOfCoffee >= this.expressoVolume) {
+      this.litersOfCoffee = this.litersOfCoffee - this.expressoVolume;
+      //this.litersOfCoffee -= this.expressoVolume;
+      return true;
+    } else {
+      return false;
+    }
+
+  },
+  longCoffee : function (){
+    if (this.litersOfCoffee >= this.longCoffeeVolume) {
+      this.litersOfCoffee = this.litersOfCoffee - this.longCoffeeVolume;
+      //this.litersOfCoffee -= this.longCoffeeVolume;
+      return true;
+    } else {
+      return false;
+    }
+
+  },
+
+};
+
+
+
 
 machine.fillWithLitersOfCoffee(10);
-console.log(machine.expresso()) // => true
-console.log(machine.litersOfCoffee) // => 9.92
-console.log(machine.longCoffee()) // => true
-console.log(machine.litersOfCoffee) // => 9.77
+
+
+
+console.log(machine.expresso()); // => true
+console.log(machine.litersOfCoffee); // => 9.92
+console.log(machine.longCoffee()); // => true
+console.log(machine.litersOfCoffee); // => 9.77
 // some more people ordering coffee here
-console.log(machine.litersOfCoffee) // => 0.02
-console.log(machine.expresso()) // => false
+console.log(machine.litersOfCoffee); // => 0.02
+console.log(machine.expresso()); // => false
 
 module.exports = machine;
